64e302fec20eecce17b68bdcb751df332c086cbf
mahadi-devo <mahadihassan083@gmail.com>
2022-05-14 19:01:24 +0530

    card component add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {Badge, ListGroup} from "react-bootstrap";
 
 function App() {
+
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/carts/1')
+        .then(res=>res.json())
+        .then(cart=>setCart(cart.products))
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="p-5">Cart</h1>
+      <ListGroup as="ol" numbered className="m-5">
+        {
+          cart.map(product => (
+              <ListGroup.Item
+                  key={product.productId}
+                  as="li"
+                  className="d-flex justify-content-between align-items-start"
+              >
+                  <div className="ms-2 me-auto">
+                      <div className="fw-bold">product {product.productId}</div>
+                  </div>
+                  <Badge bg="primary" pill>
+                      quantity: {product.quantity}
+                  </Badge>
+              </ListGroup.Item>
+          ))
+        }
+      </ListGroup>
     </div>
   );
 }
